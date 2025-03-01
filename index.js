b4c9890dd2dcd1165c9d3d1982121497e8183d57
mdiffatbari <iffatbaribappy1999@gmail.com>
2025-03-02 00:39:59 +0600

    all design done

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const specialButton = document.getElementById('bg-color-change-btn');
 specialButton.addEventListener('click', function(){
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = '#' + randomColor;
+});
+
+
+const blogButton = document.getElementById('blog-btn');
+
+blogButton.addEventListener('click', function(){
+    window.location.href = 'blog.html';
 })
